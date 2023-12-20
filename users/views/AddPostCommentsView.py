d2a58db7302fe6fde9bf47380c5046f160ae3c98
fahim <therunninghorse00@gmail.com>
2023-12-21 04:16:15 +0600

    small import clean up

--- users/views/AddPostCommentsView.py
+++ users/views/AddPostCommentsView.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from users.models import Post
 from users.serializers import PostAddCommentSerializer, ProfileInfoSerializer
-from django.db.models import F
 from django.shortcuts import get_object_or_404
 
 
