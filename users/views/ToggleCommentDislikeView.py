10dfc330261c8d608fdf60638602ec7bf22618bd
fahim <fahim.x@yahoo.com>
2024-01-02 08:56:53 +0600

    feat: Add comment dislike toggle
    This commit adds the feature to toggle comment dislike.

---
+++ users/views/ToggleCommentDislikeView.py
@@ -0,0 +1,38 @@
+from uuid import UUID
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from users.models import Comment
+from django.shortcuts import get_object_or_404
+
+
+class ToggleCommentDislikeView(APIView):
+    def post(self, request):
+        id = request.data.get("id", None)
+        if id:
+            try:
+                request.data["id"] = UUID(request.data["id"])
+            except:
+                pass
+
+        comment = get_object_or_404(Comment, id=id)
+
+        hasDisLiked = comment.dislikes.filter(id=request.user.profile.id).exists()
+
+        hasLiked = comment.likes.filter(id=request.user.profile.id).exists()
+
+        if hasDisLiked:
+            comment.dislikes.remove(request.user.profile)
+            comment.dislike_counts = comment.dislike_counts - 1
+            comment.save()
+        else:
+            comment.dislikes.add(request.user.profile)
+            comment.dislike_counts = comment.dislike_counts + 1
+            comment.save()
+
+        if hasLiked:
+            comment.likes.remove(request.user.profile)
+            comment.like_counts = comment.like_counts - 1
+            comment.save()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
