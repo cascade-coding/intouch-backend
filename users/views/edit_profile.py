0509c75b83d58c9fd98f9e406e9b2373dbd8ff3c
fahim <fahim.x@yahoo.com>
2024-01-07 05:40:48 +0600

    feat: Add profile update feature

--- users/views/edit_profile.py
+++ users/views/edit_profile.py
@@ -8,3 +8,12 @@ class EditProfileView(APIView):
         profile = request.user.profile
         serializer = EditProfileSerializer(instance=profile)
         return Response(serializer.data)
+
+    def put(self, request):
+        profile = request.user.profile
+        serializer = EditProfileSerializer(
+            instance=profile, data=request.data, partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
